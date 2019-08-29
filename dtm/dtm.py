ec92dd9a05eefff5421738814853364bb53ee8c5
Dale Tovar <37719258+daletovar@users.noreply.github.com>
2019-08-29 09:59:19 -0700

    Update dtm.py

--- dtm/dtm.py
+++ dtm/dtm.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import nibabel as nib 
 
+from pySINDy import SINDy 
 from sklearn.decomposition import PCA
 from nilearn.input_data import NiftiMasker
 
@@ -53,16 +54,53 @@ class DTM():
             self.pca_model = pca_model
             data = pca_model.transform(data.T).T
 
+        # read onset files
+        
+        self.n_state_vars = data.shape[0]
+        data = np.vstack((data,onsets))
+
         # sindy model here
+        self.sindy_model = SINDy()
+        self.sindy_model.fit(data,self.dt,poly_degree=self.poly_degree)
+        
 
 
+        def equations(self):
+            """
+            show equations
+            """
+            coef = self.sindy_model.coefficients
+            desc = np.array(self.sindy_model.descriptions)
+            for i in range(coef.shape[1] - self.n_conditions):
+                eq = 'u{}/dt = '.format(i)
+                idx = np.nonzero(coef[:,i])[0]
+                for j in idx: 
+                    eq += str(coef[j,i]) + desc[j] + ' '
+                print(eq)
+
+        def desc_to_functions(self):
+            """
+            5.668(u0 * u1)**2
+            """
+            
 
         def predict(self,nifti,onsets):
             """
             integrates the system using RK4 to generate predictions
             given new onset files and initial conditions
             """
-            data = 
+            
+            # somehow get data
+            data = self.pca_model.transform(NiftiMasker(self.mask).fit_transform(nifti))
+
+            # read onset files
+
+            init_conditions = data[:,0]
+
+            funcs = [lambda]
+            for 
+
+            
 
 
         def plot_components(self):
